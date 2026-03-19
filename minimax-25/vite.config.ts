0cb35b656d9f5a262868342a7dcbd78957d9f765
Max Kaye <m@xk.io>
2026-03-19 19:59:39 +1100

    fix path issues minimax

--- minimax-25/vite.config.ts
+++ minimax-25/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/gpt-tetris-comparisons/minimax-25/',
   root: '.',
   publicDir: 'public',
   server: {
